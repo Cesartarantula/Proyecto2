165444241e50a4d5222293fc67148d08f00ce59b
Cesartarantula <cvzcesarin@gmail.com>
2019-07-07 23:25:08 -0600

    Envioalprofe2

--- LogicaMasterPrueba3-Inestable/PCIE_trans.v
+++ LogicaMasterPrueba3-Inestable/PCIE_trans.v
@@ -35,9 +35,9 @@ module PCIE_trans (//Entradas
 //conexiones internas del top
  wire Almost_Empty_MF, Almost_Full_MF, Pausa_MF, Fifo_Empty_MF, Fifo_Full_MF, Error_Fifo_MF;
 
-wire push_vc0, push_vc1, push_Demux, Pausa_VC0, Pausa_VC1, Fifo_Empty_VC0, Fifo_Empty_VC1, valid_out;
+wire push_vc0, push_vc1, push_Demux, Pausa_VC0, Pausa_VC1, Fifo_Empty_VC0, Fifo_Empty_VC1, valid_out_MF, valid_out_VC0;
 
-wire [5:0] Fifo_Data_out_MF, data_in_VC0, dataout_VCs, data_in_D0, data_in_D1, data_in_VC1;
+wire [5:0] Fifo_Data_out_MF, data_in_VC0, dataout_VCs, data_in_D0, data_in_D1, data_in_VC1, Fifo_Data_out_VC0, Fifo_Data_out_VC1 ;
 
 wire Almost_Empty_D1, Almost_Full_D1, Pausa_D1 , Fifo_Empty_D1, Fifo_Full_D1, Error_Fifo_D1;
 
@@ -76,12 +76,12 @@ fifo #(.N(2), .ADDR_WIDTH(4)) MainFifo (	.clk(clk),
 						.Fifo_Empty(Fifo_Empty_MF),
 						.Fifo_Full(Fifo_Full_MF),
 						.Error_Fifo(Error_Fifo_MF),
-						.valid_out(valid_out)); 
+						.valid_out(valid_out_MF)); 
 
 // Modulo Demux de pop válidos según vc_id
 demux demux1 (		.clk(clk),
 			.reset_L(reset_L),
-			.valid_in(valid_out),//Modificacion
+			.valid_in(valid_out_MF),//Modificacion
 			.data_in(Fifo_Data_out_MF),
 			.dataout0(data_in_VC0),
 			.dataout1(data_in_VC1),
@@ -102,7 +102,8 @@ fifo #(.N(4), .M(2), .ADDR_WIDTH(16)) VC0Fifo (.clk(clk),
 						                       .Pausa(Pausa_VC0),
 						                       .Fifo_Empty(Fifo_Empty_VC0),
 						                       .Fifo_Full(Fifo_Full_VC0),
-						                       .Error_Fifo(Error_Fifo_VC0)); 
+						                       .Error_Fifo(Error_Fifo_VC0),
+								       .valid_out(valid_out_VC0)); 
 
 //VC1: FIFO (6bitsx16)
 fifo #(.N(4), .M(2), .ADDR_WIDTH(16)) VC1Fifo (		.clk(clk),
@@ -124,8 +125,8 @@ fifo #(.N(4), .M(2), .ADDR_WIDTH(16)) VC1Fifo (		.clk(clk),
 //de datos de VC0.
 mux mux1 (	.clk(clk),
 		.reset_L(reset_L),
-		.valid_in_VC0(pop_vc0),
-		.valid_in_VC1(pop_vc1),
+		.valid_in_VC0(valid_out_VC0),
+		.valid_in_VC1(pop_vc0),// (pop_vc1) 
 		.data_in_VC0(Fifo_Data_out_VC0),
 		.data_in_VC1(Fifo_Data_out_VC1),
 		.dataout(dataout_VCs),
